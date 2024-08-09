97476633055ecb5559c7442298fbc86ab16b7817
Chinmay Tiwari <tiwarichinmay121@gmail.com>
2024-08-10 03:04:53 +0530

    Gigs card updated

--- client/src/pages/gigs/Gigs.jsx
+++ client/src/pages/gigs/Gigs.jsx
@@ -1,45 +1,20 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./Gigs.scss";
 import GigCard from "../../components/gigCard/GigCard";
-import { useQuery } from "@tanstack/react-query";
-import newRequest from "../../utils/newRequest";
-import { useLocation } from "react-router-dom";
+// import { useQuery } from "@tanstack/react-query";
+// import newRequest from "../../utils/newRequest";
+// import { useLocation } from "react-router-dom";
+import {gigs} from "../../data";
 
-function Gigs() {
-  const [sort, setSort] = useState("sales");
+const Gigs = () => {
+  const [sort, setSort] = useState(false);
   const [open, setOpen] = useState(false);
-  const minRef = useRef();
-  const maxRef = useRef();
-
-  const { search } = useLocation();
-
-  const { isLoading, error, data, refetch } = useQuery({
-    queryKey: ["gigs"],
-    queryFn: () =>
-      newRequest
-        .get(
-          `/gigs${search}&min=${minRef.current.value}&max=${maxRef.current.value}&sort=${sort}`
-        )
-        .then((res) => {
-          return res.data;
-        }),
-  });
-
-  console.log(data);
 
   const reSort = (type) => {
+    console.log(type);
     setSort(type);
     setOpen(false);
-  };
-
-  useEffect(() => {
-    refetch();
-  }, [sort]);
-
-  const apply = () => {
-    refetch();
-  };
-
+  }
   return (
     <div className="gigs">
       <div className="container">
@@ -51,35 +26,32 @@ function Gigs() {
         <div className="menu">
           <div className="left">
             <span>Budget</span>
-            <input ref={minRef} type="number" placeholder="min" />
-            <input ref={maxRef} type="number" placeholder="max" />
-            <button onClick={apply}>Apply</button>
+            <input type="number" placeholder="min" />
+            <input type="number" placeholder="max" />
+            <button >Apply</button>
           </div>
-          <div className="right">
+          <div className="right" >
             <span className="sortBy">Sort by</span>
-            <span className="sortType">
-              {sort === "sales" ? "Best Selling" : "Newest"}
-            </span>
-            <img src="./img/down.png" alt="" onClick={() => setOpen(!open)} />
-            {open && (
-              <div className="rightMenu">
-                {sort === "sales" ? (
-                  <span onClick={() => reSort("createdAt")}>Newest</span>
-                ) : (
-                  <span onClick={() => reSort("sales")}>Best Selling</span>
-                )}
-                <span onClick={() => reSort("sales")}>Popular</span>
-              </div>
-            )}
+            <div onClick={() => setOpen(!open)}>
+              <span className="sortType">
+                {sort === "sales" ? "Best Selling" : "Newest"}
+              </span>
+              <img src="./img/down.png" alt="" />
+              {open && <div className="rightMenu">
+                {sort === "sales" ? (<span onClick={() => reSort("createdAt")}>Newest</span>) :
+                  (<span onClick={() => reSort("sales")}>Best Selling</span>)}
+              </div>}
+            </div>
+
+
           </div>
         </div>
         <div className="cards">
-          {isLoading
-            ? "loading"
-            : error
-            ? "Something went wrong!"
-            : data.map((gig) => <GigCard key={gig._id} item={gig} />)}
+                {gigs.map(gig =>(
+                  <GigCard key = {gig.id} item={gig} />
+                ))}
         </div>
+
       </div>
     </div>
   );
